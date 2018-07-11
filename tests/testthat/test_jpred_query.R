c41c021575a9916ea55d5b30c4014266a4dfc486
Andrey Smelter <andrey.smelter@gmail.com>
2018-07-10 23:48:08 -0400

    Removed example test.

--- tests/testthat/test_jpred_query.R
+++
@@ -1,8 +0,0 @@
-context("test-jpred_parameters")
-
-example_file_loc <- file.path(rprojroot::find_root("DESCRIPTION"), "inst", "exampledata")
-
-test_that("batch fasta works", {
-  out_param <- create_jpred_query(mode="batch", user_format="fasta", file = file.path(example_file_loc, "batch_fasta.example"), email="name@domain.com", silent = TRUE)
-  expect_known_output(out_param, file = "fasta_batch", print = TRUE)
-})
