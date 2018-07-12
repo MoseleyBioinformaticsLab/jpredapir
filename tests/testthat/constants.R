1dc9b8e4d1173e9cd18e23df3af504cfae322f02
Robert M Flight <r.flight79@gmail.com>
2018-07-12 10:12:13 -0400

    using the system.file function to get at package data needed for testing and the CLI.

--- tests/testthat/constants.R
+++ tests/testthat/constants.R
@@ -1,5 +1,5 @@
 SKIP_REAL_JPREDAPI <- TRUE
-EXAMPLE_DATA_DIR <- file.path(rprojroot::find_root("DESCRIPTION"), "inst", "exampledata")
+EXAMPLE_DATA_DIR <- system.file("exampledata", package = "jpredapir")
 HOST <- "http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest"
 JPRED4 <- "http://www.compbio.dundee.ac.uk/jpred4"
-CLI_EXEC <- shQuote(file.path(rprojroot::find_root("DESCRIPTION"), "exec", "jpredapi.R"))
+CLI_EXEC <- shQuote(system.file("exec", "jpredapi.R", package = "jpredapir"))
