33e69da82ab038fd95783713d8be4248c9841a86
Andrey Smelter <andrey.smelter@gmail.com>
2018-07-17 12:15:14 -0400

    Clean up "jobid" variable.

--- tests/testthat/test_status.R
+++ tests/testthat/test_status.R
@@ -8,8 +8,8 @@ testthat::test_that("test status API call", {
   
   submit_response <- jpredapir::submit(mode="single", user_format="raw", seq="MQVWPIEGIKKFETLSYLPP")
   result_url <- httr::headers(submit_response)$location
-  job_id <- stringr::str_match(string = result_url, pattern = "(jp_.*)$")[2]
-  status_response <- jpredapir::status(job_id = job_id, results_dir_path = NULL, extract = FALSE, silent = FALSE,  host = HOST, jpred4 = JPRED4)
+  jobid <- stringr::str_match(string = result_url, pattern = "(jp_.*)$")[2]
+  status_response <- jpredapir::status(jobid = jobid, results_dir_path = NULL, extract = FALSE, silent = FALSE,  host = HOST, jpred4 = JPRED4)
   
   testthat::expect_equal(status_response$status_code, 200)
 })
@@ -20,8 +20,8 @@ testthat::test_that("test status API call", {
   
   submit_response <- jpredapir::submit(mode="single", user_format="raw", seq="MQVWPIEGIKKFETLSYLPP")
   result_url <- httr::headers(submit_response)$location
-  job_id <- stringr::str_match(string = result_url, pattern = "(jp_.*)$")[2]
-  status_response <- jpredapir::status(job_id = job_id, results_dir_path = "jpred_results", extract = FALSE, silent = FALSE, host = HOST, jpred4 = JPRED4)
+  jobid <- stringr::str_match(string = result_url, pattern = "(jp_.*)$")[2]
+  status_response <- jpredapir::status(jobid = jobid, results_dir_path = "jpred_results", extract = FALSE, silent = FALSE, host = HOST, jpred4 = JPRED4)
   
   testthat::expect_equal(status_response$status_code, 200)
 })
@@ -32,8 +32,8 @@ testthat::test_that("test status API call", {
   
   submit_response <- jpredapir::submit(mode="single", user_format="raw", seq="MQVWPIEGIKKFETLSYLPP")
   result_url <- httr::headers(submit_response)$location
-  job_id <- stringr::str_match(string = result_url, pattern = "(jp_.*)$")[2]
-  status_response <- jpredapir::status(job_id = job_id, results_dir_path = "jpred_results", extract = TRUE, silent = FALSE, host = HOST, jpred4 = JPRED4)
+  jobid <- stringr::str_match(string = result_url, pattern = "(jp_.*)$")[2]
+  status_response <- jpredapir::status(jobid = jobid, results_dir_path = "jpred_results", extract = TRUE, silent = FALSE, host = HOST, jpred4 = JPRED4)
   
   testthat::expect_equal(status_response$status_code, 200)
 })
@@ -44,8 +44,8 @@ testthat::test_that("test status API call", {
 testthat::test_that("test mock status API call", {
   testthat::with_mock(
     
-    status = function(job_id, results_dir_path, extract, silent, host, jpred4) {return(list(success = TRUE, status_code = 200))},
-    status_response <- status(job_id = "jp_mock", results_dir_path = NULL, extract = FALSE, silent = FALSE, host = HOST, jpred4 = JPRED4),
+    status = function(jobid, results_dir_path, extract, silent, host, jpred4) {return(list(success = TRUE, status_code = 200))},
+    status_response <- status(jobid = "jp_mock", results_dir_path = NULL, extract = FALSE, silent = FALSE, host = HOST, jpred4 = JPRED4),
     
     testthat::expect_equal(status_response$status_code, 200))
 })
@@ -54,8 +54,8 @@ testthat::test_that("test mock status API call", {
 testthat::test_that("test mock status API call", {
   testthat::with_mock(
     
-    status = function(job_id, results_dir_path, extract, silent, host, jpred4) {return(list(success = TRUE, status_code = 200))},
-    status_response <- status(job_id = "jp_mock", results_dir_path = "jpred_results", extract = FALSE, silent = FALSE, host = HOST, jpred4 = JPRED4),
+    status = function(jobid, results_dir_path, extract, silent, host, jpred4) {return(list(success = TRUE, status_code = 200))},
+    status_response <- status(jobid = "jp_mock", results_dir_path = "jpred_results", extract = FALSE, silent = FALSE, host = HOST, jpred4 = JPRED4),
     
     testthat::expect_equal(status_response$status_code, 200))
 })
@@ -64,8 +64,8 @@ testthat::test_that("test mock status API call", {
 testthat::test_that("test mock status API call", {
   testthat::with_mock(
     
-    status = function(job_id, results_dir_path, extract, silent, host, jpred4) {return(list(success = TRUE, status_code = 200))},
-    status_response <- status(job_id = "jp_mock", results_dir_path = "jpred_results", extract = TRUE, silent = FALSE, host = HOST, jpred4 = JPRED4),
+    status = function(jobid, results_dir_path, extract, silent, host, jpred4) {return(list(success = TRUE, status_code = 200))},
+    status_response <- status(jobid = "jp_mock", results_dir_path = "jpred_results", extract = TRUE, silent = FALSE, host = HOST, jpred4 = JPRED4),
     
     testthat::expect_equal(status_response$status_code, 200))
 })
